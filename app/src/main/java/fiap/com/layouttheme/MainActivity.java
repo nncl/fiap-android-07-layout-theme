879efd6abc794608fb2b5ec633d7b498bf99b66f
Caue Bruno de Almeida <RM30917@fiap.com.br>
2016-10-31 20:27:28 -0200

    Initial commit + building custom layout

---
+++ app/src/main/java/fiap/com/layouttheme/MainActivity.java
@@ -0,0 +1,12 @@
+package fiap.com.layouttheme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
